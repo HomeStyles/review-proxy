e8c7b2322169e52b53a3e352fec8a178afb63a21
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-04-24 16:27:03 -0700

    Get everything to work

--- proxy/server.js
+++ proxy/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3010;
 
 app.use(cors());
 app.use(morgan('dev'));
